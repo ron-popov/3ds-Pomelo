29c37dfbf07d7e13f22ddca2c29e3e8801874adb
Ron Popov <ron200011@gmail.com>
2026-05-14 19:52:28 +0300

    Changed patching decoys to use ASM word statements
    I had issues when i tried running it on real hardware, with exheaders that have the correct section sizes, becuase the loader looks specifically in the text section

--- source/main.c
+++ source/main.c
@@ -19,26 +19,28 @@ static char* PATCHING_BYPASS_REGION_FREE_PATCHING __attribute__((used)) =
     "\x0A\x0C\x00\x10"
     "\x00\x00\x00\x00";
 
-static char* PATCHING_BYPASS_SMDH_REGION_CHECK_MANUAL __attribute__((used)) = 
-    "\x00\x00\x11\xE1"
-    "\x00\x00\x00\x0A"
-    "\x0D\x00\xA0\xE1";
-
-// This is repeated 4 times to make sure atleast 1 of the section is in the 4 offset
-static char* PATCHING_BYPASS_NDS_FLASHCARD_CHECK __attribute__((used)) = 
-    "\x2D\xE9\xAA\xAA\xAA\xAA\xBB\xBB\xBB\xBB\xFF"
-    "\x10\xD1\xE5\x08\x00\x8D"
-    "\x00"
-    "\x2D\xE9\xAA\xAA\xAA\xAA\xBB\xBB\xBB\xBB\xFF"
-    "\x10\xD1\xE5\x08\x00\x8D"
-    "\x00"
-    "\x2D\xE9\xAA\xAA\xAA\xAA\xBB\xBB\xBB\xBB\xFF"
-    "\x10\xD1\xE5\x08\x00\x8D"
-    "\x00"
-    "\x2D\xE9\xAA\xAA\xAA\xAA\xBB\xBB\xBB\xBB\xFF"
-    "\x10\xD1\xE5\x08\x00\x8D";
 
 
+// 0x10, 0xD1, 0xE5, 0x08, 0x00, 0x8D
+__attribute__((naked)) void inject_loader_decoys(void) {
+    asm volatile (
+        "b 1f\n"                    // Jump to label '1' (forward)
+        ""                          // SMDH REGION CHECK MANUAL START
+        ".word 0xE1110000\n"
+        ".word 0x0A000000\n"
+        ".word 0xE1A0000D\n"
+        ""                          // SMDH REGION CHECK MANUAL END
+        ""                          // NDS FLASHCARD WHITELIST START
+        ".word 0x00E92DAA\n"
+        ".word 0xAAAAAABB\n"
+        ".word 0xBBBBBBFF\n"
+        ".word 0x08E5D110\n"
+        ".word 0x00008D00\n"
+        ""                          // NDS FLASHCARD WHITELIST END
+        "1:\n"                      // Label '1'
+        "bx lr\n"                   // Return
+    );
+}
 
 
 #define SCREEN_WIDTH 400
@@ -254,6 +256,8 @@ void _aptDebug(int a, int b) {
 }
 
 int main(int argc, char* argv[]) {
+
+    inject_loader_decoys();
 
     gfxInitDefault();
 
