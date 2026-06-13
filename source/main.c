fafeb021484c9f108c8a3219ee82a939bb658cdd
Ron Popov <ron200011@gmail.com>
2026-06-13 16:37:35 +0300

    WIP: translate current UI to citro2d

--- source/main.c
+++ source/main.c
@@ -1,6 +1,6 @@
 #include <3ds.h>
 #include <citro3d.h>
-#include "citro2d/citro2d.h"
+#include <citro2d.h>
 
 #include <string.h>
 #include <stdio.h>
@@ -57,6 +57,9 @@ u32 __ctru_linear_heap_size = 0xb64000;
 static aptHookCookie homemenuAptHookCookie;
 static PrintConsole topScreen;
 
+static inline u32 rgb_to_C2D_Color32(u32 color) {
+    return C2D_Color32(get_red(color), get_blue(color), get_green(color), 0xff);
+}
 
 // This handles apt callbacks to our process
 void aptCallback(APT_HookType hook, void* param) {
@@ -142,14 +145,11 @@ int main(int argc, char* argv[]) {
     inject_loader_decoys();
 
 
-
-
     // Init gfx stuff
     gfxInitDefault();
     // gfxInit(GSP_RGBA8_OES, GSP_RGBA8_OES, false);
 
 	consoleInit(GFX_TOP, &topScreen);
-
     consoleSelect(&topScreen);
 
     SetState(STATE_FOREGROUND);
@@ -444,60 +444,28 @@ int main(int argc, char* argv[]) {
     bool is_first_run = true;
     int scroll_offset = 0;
 
+    // Init rendering stuff
+    C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+	C2D_Prepare();
+
     // Init render target
     C3D_RenderTarget* bottom = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
+    if (bottom == NULL) {
+        log_debug("Failed initializing bottom screen for rendering!");
+        return 0;
+    }
 
-    // Create colors
-	u32 clrWhite = C2D_Color32(0xFF, 0xFF, 0xFF, 0xFF);
-	u32 clrGreen = C2D_Color32(0x00, 0xFF, 0x00, 0xFF);
-	u32 clrRed   = C2D_Color32(0xFF, 0x00, 0x00, 0xFF);
-	u32 clrBlue  = C2D_Color32(0x00, 0x00, 0xFF, 0xFF);
-
-	u32 clrCircle1 = C2D_Color32(0xFF, 0x00, 0xFF, 0xFF);
-	u32 clrCircle2 = C2D_Color32(0xFF, 0xFF, 0x00, 0xFF);
-	u32 clrCircle3 = C2D_Color32(0x00, 0xFF, 0xFF, 0xFF);
-
-	u32 clrSolidCircle = C2D_Color32(0x68, 0xB0, 0xD8, 0xFF);
-
-	u32 clrTri1 = C2D_Color32(0xFF, 0x15, 0x00, 0xFF);
-	u32 clrTri2 = C2D_Color32(0x27, 0x69, 0xE5, 0xFF);
-
-	u32 clrRec1 = C2D_Color32(0x9A, 0x6C, 0xB9, 0xFF);
-	u32 clrRec2 = C2D_Color32(0xFF, 0xFF, 0x2C, 0xFF);
-	u32 clrRec3 = C2D_Color32(0xD8, 0xF6, 0x0F, 0xFF);
-	u32 clrRec4 = C2D_Color32(0x40, 0xEA, 0x87, 0xFF);
-
-	u32 clrClear = C2D_Color32(0xFF, 0xD8, 0xB0, 0x68);
-
-    log_debug("Starting to render");
-
-    // Render the scene
-    C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-    C2D_TargetClear(bottom, clrClear);
-    C2D_SceneBegin(bottom);
-
-    C2D_DrawTriangle(50 / 2, BOTTOM_SCREEN_HEIGHT - 50, clrWhite, 
-        0,  BOTTOM_SCREEN_HEIGHT, clrTri1,
-        50, BOTTOM_SCREEN_HEIGHT, clrTri2, 0);
-    C2D_DrawRectangle(BOTTOM_SCREEN_WIDTH - 50, 0, 0, 50, 50, clrRec1, clrRec2, clrRec3, clrRec4);
-
-    // Circles require a state change (an expensive operation) within citro2d's internals, so draw them last.
-    // Although it is possible to draw them in the middle of drawing non-circular objects
-    // (sprites, images, triangles, rectangles, etc.) this is not recommended. They should either
-    // be drawn before all non-circular objects, or afterwards.
-    C2D_DrawEllipse(0, 0, 0, BOTTOM_SCREEN_WIDTH, BOTTOM_SCREEN_HEIGHT, clrCircle1, clrCircle2, clrCircle3, clrWhite);
-    C2D_DrawCircle(BOTTOM_SCREEN_WIDTH / 2, BOTTOM_SCREEN_HEIGHT / 2, 0, 50, clrCircle3, clrWhite, clrCircle1, clrCircle2);
-    C2D_DrawCircle(25, 25, 0, 25, 
-        clrRed, clrBlue, clrGreen, clrWhite);
-    C2D_DrawCircleSolid(BOTTOM_SCREEN_WIDTH - 25, BOTTOM_SCREEN_HEIGHT - 25, 0, 25, clrSolidCircle);
-    C3D_FrameEnd(0);
-
-    log_debug("Finished rendering");
+    // Load font
+    C2D_Font font = C2D_FontLoadSystem(CFG_REGION_USA);
+    // C2D_Font font = C2D_FontLoadFromMem(&font, 96*13); // system font
+    // or load a custom font:
+    // C2D_Font font = C2D_FontLoad("romfs:/myfont.bcfnt");
 
     while(true) {
 
-        aptMainLoop();     
-
+        aptMainLoop();
+        
         switch (GetState()) {
             case STATE_NONE:
                 log_debug("Invalid state reached");
@@ -684,6 +652,51 @@ int main(int argc, char* argv[]) {
                     gfxFlushBuffers();
                     gfxSwapBuffers();
                     gspWaitForVBlank();
+                }
+
+                {
+                    log_debug("Starting to render");
+
+                    // Render the scene
+                    C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+                    C2D_TargetClear(bottom, rgb_to_C2D_Color32(COL_BG));
+                    C2D_SceneBegin(bottom);
+
+                    log_debug("Rendering header text");
+
+                    // Render the header text - app name
+                    C2D_TextBuf buf = C2D_TextBufNew(4096); // glyph buffer
+
+                    log_debug("Building text buffer");
+
+                    C2D_Text text;
+                    C2D_TextFontParse(&text, font, buf, "Hello from Pomelo");
+                    C2D_TextOptimize(&text);
+
+                    log_debug("Drawing text");
+
+                    C2D_DrawText(&text, C2D_WithColor, 10, 10, 0, 1, 1, rgb_to_C2D_Color32(COL_TEXT));
+
+                    // C2D_DrawTriangle(0, 0, clrWhite, 
+                    //     100,  0, clrTri1,
+                    //     50, 50, clrTri2,
+                    //     0
+                    // );
+                    // C2D_DrawRectangle(BOTTOM_SCREEN_WIDTH - 50, 0, 0, 50, 50, clrRec1, clrRec2, clrRec3, clrRec4);
+                    // C2D_DrawRectangle(0, 0, 0, 50, 50, clrRec1, clrRec2, clrRec3, clrRec4);
+
+                    // // Circles require a state change (an expensive operation) within citro2d's internals, so draw them last.
+                    // // Although it is possible to draw them in the middle of drawing non-circular objects
+                    // // (sprites, images, triangles, rectangles, etc.) this is not recommended. They should either
+                    // // be drawn before all non-circular objects, or afterwards.
+                    // C2D_DrawEllipse(0, 0, 0, BOTTOM_SCREEN_WIDTH, BOTTOM_SCREEN_HEIGHT, clrCircle1, clrCircle2, clrCircle3, clrWhite);
+                    // C2D_DrawCircle(BOTTOM_SCREEN_WIDTH / 2, BOTTOM_SCREEN_HEIGHT / 2, 0, 50, clrCircle3, clrWhite, clrCircle1, clrCircle2);
+                    // C2D_DrawCircle(25, 25, 0, 25, 
+                    //     clrRed, clrBlue, clrGreen, clrWhite);
+                    // C2D_DrawCircleSolid(BOTTOM_SCREEN_WIDTH - 25, BOTTOM_SCREEN_HEIGHT - 25, 0, 25, clrSolidCircle);
+                    C3D_FrameEnd(0);
+
+                    log_debug("Finished rendering");
                 }
             }
         }
